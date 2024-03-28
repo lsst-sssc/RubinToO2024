2be90255bfbafe0c001459c166262fa526d31548
Tim Lister <tlister@lco.global>
2024-03-28 13:57:56 -0700

    Add tests for default get_m5() routine before twilight mods

--- tests/test_etc.py
+++ tests/test_etc.py
@@ -3,12 +3,13 @@ import pytest
 import numpy as np
 from numpy.testing import assert_allclose
 
-from etc import get_exptime
+from etc import get_exptime, get_m5
 
 class TestEtc:
     @pytest.fixture(autouse=True)
     def setUp(self):
         self.m5s = [23.70, 24.97, 24.52, 24.13, 23.56, 22.55]
+        self.exptime = 30.0
         self.filters_all = "ugrizy"
 
     def test_darksky_exptime_zenith(self):
@@ -62,3 +63,29 @@ class TestEtc:
         for m5, filt in zip(self.m5s, self.filters_all):
             exptime_out = get_exptime(m5, filt, X=2.92, twilight=True)
             assert_allclose(exptime_out, expected_exptimes[filt], rtol=1e-4)
+
+    def test_darksky_m5_zenith(self):
+        expected_m5s = { 'u': 23.6983,
+                         'g': 24.9689,
+                         'r': 24.5150,
+                         'i': 24.1250,
+                         'z': 23.5575,
+                         'y': 22.5507
+                       }
+
+        for filt in self.filters_all:
+            m5_out = get_m5(self.exptime, filt, X=1.)
+            assert_allclose(m5_out, expected_m5s[filt], rtol=1e-4)
+
+    def test_darksky_m5_am12(self):
+        expected_m5s = { 'u': 23.6043,
+                         'g': 24.9269,
+                         'r': 24.4890,
+                         'i': 24.1050,
+                         'z': 23.5435,
+                         'y': 22.5167
+                       }
+
+        for filt in self.filters_all:
+            m5_out = get_m5(self.exptime, filt, X=1.2)
+            assert_allclose(m5_out, expected_m5s[filt], rtol=1e-4)
